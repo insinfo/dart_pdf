46f6e8679bf7c745392d8f8ca6d43dfdab581a39
Isaque Neves <isaque.neves@riodasostras.rj.gov.br>
2023-08-24 18:23:09 -0300

    fix

---
+++ merge_from_upstream.ps1
@@ -0,0 +1,6 @@
+#https://stackoverflow.com/questions/7244321/how-do-i-update-or-sync-a-forked-repository-on-github
+#git remote add upstream https://github.com/whoever/whatever.git
+git fetch upstream
+#git merge upstream/master   https://pspdfkit.com/blog/2020/updating-pdfium/
+#git imerge upstream/master  
+git merge upstream/master  
